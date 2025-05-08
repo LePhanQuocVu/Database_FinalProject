7e3b2564a0fcae7d3dbe536e6baee9757f544f98
Vunguyen222 <vunguyenking@gmail.com>
2025-05-08 23:43:11 +0700

    billController change

--- src/controller/billController.js
+++ src/controller/billController.js
@@ -2,31 +2,32 @@ const db = require('../config/dbConnect');
 const sql = require('mssql');
 
 async function insertBill(req, res) {
-    try {
-        console.log(req.body);
-        const {
-            paidDate,
-            customerPhone,
-            saleSSN,
-            totalPrice,
-            storeID,
-            listOfProduct,
-        } = req.body;
+    // console.log(req.body);
+    const {
+        paidDate,
+        customerPhone,
+        saleSSN,
+        totalPrice,
+        storeID,
+        listOfProduct,
+    } = req.body;
 
-        (await db.poolConnect)
-            .request()
-            .input('paid_Date', sql.Date, paidDate)
-            .input('card_ID', sql.Char(10), customerPhone)
-            .input('sale_SSN', sql.Char(12), saleSSN)
-            .input('total_Price', sql.Decimal(10, 3), totalPrice)
-            .input('store_ID', sql.Int, storeID)
-            .input('list_Of_Product', sql.VarChar(sql.MAX), listOfProduct)
-            .execute('INSERT_NEW_BILL');
-        return res.status(200).json({ state: 'success' });
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json(err);
-    }
+    // console.log(paidDate);
+    (await db.poolConnect)
+        .request()
+        .input('paid_Date', sql.Date, new Date(paidDate))
+        .input('card_ID', sql.Char(10), customerPhone)
+        .input('sale_SSN', sql.Char(12), saleSSN)
+        .input('total_Price', sql.Decimal(10, 3), totalPrice)
+        .input('store_ID', sql.Int, storeID)
+        .input('list_Of_Product', sql.VarChar(sql.MAX), listOfProduct)
+        .execute('INSERT_NEW_BILL')
+        .then((result) => {
+            return res.status(200).json({ state: 'success' });
+        })
+        .catch((err) => {
+            return res.status(500).json(err.message);
+        });
 }
 
 module.exports = { insertBill };
