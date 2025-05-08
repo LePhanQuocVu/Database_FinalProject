6e01d80cba902ee43b8a0cbce83c37b244c15e3c
LePhanQuocVu <vulephanquoc@gmail.com>
2025-05-09 06:34:58 +0700

    resolve conflict

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 const express = require('express');
 
-<<<<<<< HEAD
-const  supplierRouter = require('./routers/supplierRoutes');
+
 const billRouter = require('./routers/billRoutes');
 const productRouter = require('./routers/productRoutes');
 const orderRouter = require('./routers/orderRoutes');
-=======
 const supplierRouter = require('./routers/supplierRoutes');
-const billRouter = require('./routers/billRoutes');
 const storeRouter = require('./routers/storeRoutes');
 const customerRouter = require('./routers/customerRoutes');
->>>>>>> 202effcd583a203184c84c106f0a654f3011207a
 
 const cors = require('cors');
 const db = require('./config/dbConnect');
@@ -32,13 +28,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/suppliers', supplierRouter);
 app.use('/api/bills', billRouter);
-<<<<<<< HEAD
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
-=======
 app.use('/api/stores', storeRouter);
 app.use('/api/customers', customerRouter);
->>>>>>> 202effcd583a203184c84c106f0a654f3011207a
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
