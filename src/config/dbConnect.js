ad8ede7da4656e7bc4dcce2545330c6f59abad72
Vunguyen222 <vunguyenking@gmail.com>
2025-05-08 16:32:29 +0700

    config db

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,27 +1,41 @@
+// lib/db.ts
+const sql = require('mssql');
 
-import sql from 'mssql'
 const config = {
-  user: '',
-  password: '',
-  server: '', // ví dụ: 'localhost' hoặc 'QUOCVUSQLEXPRESS07'
-  database: '',
-  port: 1433,
-  options: {
-    encrypt: false, // true nếu dùng Azure
-    trustServerCertificate: true,
-  },
+    server: 'localhost',
+    database: 'HighlandsCoffee',
+    user: 'sa',
+    password: '123456',
+    options: {
+        encrypt: false,
+        trustServerCertificate: true,
+    },
+};
+
+// export async function query<T = any>(sqlQuery: string): Promise<T[]> {
+//     try {
+//         const pool = await sql.connect(config);
+//         const result = await pool.request().query(sqlQuery);
+//         return result.recordset;
+//     } catch (err) {
+//         throw err;
+//     } finally {
+//         // await sql.close()
+//     }
+// }
+
+const pool = new sql.ConnectionPool(config);
+const poolConnect = pool.connect();
+
+async function getConnection() {
+    try {
+        await poolConnect; // đảm bảo đã kết nối trước khi trả về pool
+        console.log('SQL connected');
+        return pool;
+    } catch (err) {
+        console.error('SQL Server Connection Failed!', err);
+        throw err;
+    }
 }
 
-const pool = new sql.ConnectionPool(config)
-const poolConnect = pool.connect()
-
-export async function getConnection() {
-  try {
-    await poolConnect // đảm bảo đã kết nối trước khi trả về pool
-    return pool
-  } catch (err) {
-    console.error('SQL Server Connection Failed!', err)
-    throw err
-  }
-}
-
+module.exports = getConnection;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
 const express = require('express');
 
-const supplierRouter = require('./routers/supplierRoutes')
+const supplierRouter = require('./routers/supplierRoutes');
+const db = require('./config/dbConnect');
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     console.log('start');
-  res.send("Hello Node.js!");
+    res.send('Hello Node.js!');
 });
 
 // Router
+db();
+app.use(express.json());
 
-app.use(express.json())
-
-
-app.use("/api/suppliers", supplierRouter)
-
+app.use('/api/suppliers', supplierRouter);
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
-
-
